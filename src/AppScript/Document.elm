4c6852a470a85e4b25f440e0664226a4574e5339
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-05-23 15:31:58 +0200

    next

---
+++ src/AppScript/Document.elm
@@ -0,0 +1,22 @@
+module AppScript.Document exposing (..)
+
+import JavaScript
+import Json.Decode
+import Json.Encode
+import Task
+
+
+type Document
+    = Document Json.Decode.Value
+
+
+active : Task.Task JavaScript.Error (Maybe Document)
+active =
+    JavaScript.run
+        "DocumentApp.getActiveDocument()"
+        Json.Encode.null
+        (Json.Decode.oneOf
+            [ Json.Decode.null Nothing
+            , Json.Decode.map (Document >> Just) Json.Decode.value
+            ]
+        )
